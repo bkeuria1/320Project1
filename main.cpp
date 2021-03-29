eecc9c5a3003623803cb5780c8e23a668c63c612
bkeuria1 <benkeurian@gmail.com>
2021-03-29 03:30:32 +0000

    Parts 1,2

--- main.cpp
+++ main.cpp
@@ -7,20 +7,31 @@ using namespace std;
 int main(int argc, char*argv[]){
 
 	map<unsigned long long,string> pcAddress;
-	int i = 1;	
+	long long total  = 0;	
   // Temporary variables
   unsigned long long addr;
   string behavior;
   long target;
+	long taken = 0;
+	long notTaken = 0;
 	ifstream infile(argv[1]);
 	 while(infile >> std::hex >> addr >> behavior >> std::hex >> target) {
-	   cout<<i<<"."<<behavior<<endl;
+	   //cout<<i<<"."<<behavior<<endl;
 	   pcAddress.insert(pair<unsigned long long,string>(addr,behavior));
-	   i++;
+	   if(behavior == "T") {
+     		 taken++;
+   	 }else {
+      		notTaken++;
+    	   }
+	  total++;
 	  }
-	Predictor p(pcAddress);
-	cout<<pcAddress.size()<<endl;
-	cout<< p.AlwaysTaken().first<<","<<p.NeverTaken().second<<endl;
+	Predictor p(pcAddress);	
+        ofstream myfile;
+	myfile.open(argv[2]);
+	myfile<<taken<<","<<total<<";\n";
+	myfile<<notTaken<<","<<total<<";\n";
+
+
 
   return 0;
 
